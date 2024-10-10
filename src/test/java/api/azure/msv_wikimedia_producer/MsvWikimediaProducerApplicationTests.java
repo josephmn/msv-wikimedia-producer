0725e20a190dbe9357c347b9780033946e812a5b
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-10-10 12:51:36 -0500

    first commit

---
+++ src/test/java/api/azure/msv_wikimedia_producer/MsvWikimediaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.azure.msv_wikimedia_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvWikimediaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
